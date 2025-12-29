9ddcf62876b7a1a94b56038f0f410dcf4349599d
gabylumban <gabylumban@gmail.com>
2025-12-29 17:38:22 +0700

    Update UserApiStepDef.java

--- OtomasiAPI/src/test/java/stedefs/UserApiStepDef.java
+++ OtomasiAPI/src/test/java/stedefs/UserApiStepDef.java
@@ -1,51 +1,28 @@
-package stepdefs;
+package stepdef;
 
 import io.cucumber.java.en.*;
 import io.restassured.response.Response;
 import requests.UserRequest;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UserApiStepDef {
 
-    int page;
-    int userId;
     Response response;
 
-    @Given("I set page to {int}")
-    public void setPage(int p) {
-        page = p;
-    }
-
-    @Given("I set user id to {int}")
-    public void setUserId(int id) {
-        userId = id;
-    }
-
-    @When("I request GET /users")
-    public void getUsers() {
+    @Given("user requests list users on page {int}")
+    public void user_requests_list_users_on_page(Integer page) {
         response = UserRequest.getUsers(page);
     }
 
-    @When("I request GET /users/{id}")
-    public void getUserById() {
-        response = UserRequest.getUserById(userId);
+    @Then("response status code should be {int}")
+    public void response_status_code_should_be(Integer statusCode) {
+        response.then().statusCode(statusCode);
     }
 
-    @Then("I receive status code {int}")
-    public void validateStatusCode(int status) {
-        response.then().statusCode(status);
-    }
-
-    @Then("response contains users list")
-    public void validateUsersList() {
+    @Then("response body should contain users")
+    public void response_body_should_contain_users() {
         response.then().body("data", not(empty()));
     }
-
-    @Then("response page is {int}")
-    public void validatePage(int expectedPage) {
-        response.then().body("page", equalTo(expectedPage));
-    }
 }
-
