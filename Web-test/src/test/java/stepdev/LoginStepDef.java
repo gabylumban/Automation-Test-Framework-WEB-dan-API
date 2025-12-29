be8bc8bf0c2e2befb06bf40f5b6382e0468a738a
gabylumban <gabylumban@gmail.com>
2025-12-29 15:54:55 +0700

    Update LoginStepDef.java

--- Web-test/src/test/java/stepdev/LoginStepDef.java
+++ Web-test/src/test/java/stepdev/LoginStepDef.java
@@ -4,47 +4,50 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import page.LoginPage;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LoginStepDef {
 
     WebDriver driver;
+    LoginPage loginPage;
 
     @Before
     public void setUp() {
         WebDriverManager.chromedriver().setup();
+
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");          // agar browser tidak tampil
-        options.addArguments("--disable-gpu");       // rekomendasi untuk Windows
-        options.addArguments("--window-size=1920,1080"); // ukuran layar virtual
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
 
         driver = new ChromeDriver(options);
-        // driver.manage().window().maximize();  // tidak perlu karena sudah diatur window-size
+        loginPage = new LoginPage(driver);
     }
 
     @Given("user is on login page")
     public void userIsOnLoginPage() {
-        driver.get("https://www.saucedemo.com/");
+        loginPage.goToLoginPage();
     }
 
     @Given("user input username with {string}")
     public void userInputUsernameWith(String username) {
-        driver.findElement(By.id("user-name")).sendKeys(username);
+        loginPage.inputUsername(username);
     }
 
     @Given("user input password with {string}")
     public void userInputPasswordWith(String password) {
-        driver.findElement(By.id("password")).sendKeys(password);
+        loginPage.inputPassword(password);
     }
 
     @When("user click login button")
     public void userClickLoginButton() {
-        driver.findElement(By.id("login-button")).click();
+        loginPage.clickLoginButton();
     }
 
     @Then("user is on homepage")
@@ -54,8 +57,7 @@ public class LoginStepDef {
 
     @Then("user able to see error message {string}")
     public void userAbleToSeeErrorMessage(String message) {
-        String errorText = driver.findElement(By.cssSelector("[data-test='error']")).getText();
-        assertTrue(errorText.contains(message));
+        loginPage.validateErrorAppear(message);
     }
 
     @After
